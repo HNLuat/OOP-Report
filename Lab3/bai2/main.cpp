88cedab9afb96b9b5626e5465ab2ce532d50ae6b
Ho Ngoc Luat <157623198+HNLuat@users.noreply.github.com>
2024-10-15 15:09:14 +0700

    Update main.cpp

--- Lab3/bai2/main.cpp
+++ Lab3/bai2/main.cpp
@@ -1,1 +1,15 @@
-
+#include <bits/stdc++.h>
+#include "sophuc.h"
+using namespace std;
+int main()
+{
+    SoPhuc a, b;
+    cin>>a>>b;
+    cout<<"Tong: "<<a+b<<endl;
+    cout<<"Hieu: "<<a-b<<endl;
+    cout<<"Tich: "<<a*b<<endl;
+    cout<<"Thuong: "<<a/b<<endl;
+    cout<<"ss bang: "<<(a==b)<<endl;
+    cout<<"ss khac: "<<(a!=b);
+    return 0;
+}
