b8897c4a7db85ae0d94006c2c830b0766a0f959e
Ho Ngoc Luat <157623198+HNLuat@users.noreply.github.com>
2024-10-19 13:46:23 +0700

    Update main.cpp

--- Lab3/bai1/main.cpp
+++ Lab3/bai1/main.cpp
@@ -3,9 +3,18 @@
 using namespace std;
 
 int main() {
-    PhanSo a;
-    cin>>a;
-    PhanSo b(1, 1);
-    cout<<a+b;
+    PhanSo a,b;
+    cin>>a>>b;
+    cout<<"a = "<<a<<", b = "<<b<<endl;
+    cout<<"Tong: "<<a+b<<endl;
+    cout<<"Hieu: "<<a-b<<endl;
+    cout<<"Tich: "<<a*b<<endl;
+    cout<<"Thuong: "<<"a/b"<<endl;
+    cout<<"a lon hon b: "<<(a>b)<<endl;
+    cout<<"a lon hon hoac bang b: "<<(a>=b)<<endl;
+    cout<<"a be hon b: "<<(a<b)<<endl;
+    cout<<"a be hon hoac bang b: "<<(a<=b)<<endl;
+    cout<<"a bang b: "<<(a==b)<<endl;
+    cout<<"a khac b: "<<(a!=b);
     return 0;
 }
